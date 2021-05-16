d31e89d18f94af880874d4a24a1e1bc175f811f2
jenifa-moritakakun <82855669+jenifa-moritakakun@users.noreply.github.com>
2021-05-16 19:24:54 +0900

    修正６

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_root_path, only: :edit
 
   def index
